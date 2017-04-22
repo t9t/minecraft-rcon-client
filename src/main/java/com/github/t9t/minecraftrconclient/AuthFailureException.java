0f666511de2b883f321a281daade3bc92eacd790
t9t <t9t@users.noreply.github.com>
2017-04-22 21:14:28 +0200

    Add Javadocs

--- src/main/java/com/github/t9t/minecraftrconclient/AuthFailureException.java
+++ src/main/java/com/github/t9t/minecraftrconclient/AuthFailureException.java
@@ -1,5 +1,8 @@
 package com.github.t9t.minecraftrconclient;
 
+/**
+ * Exception thrown by {@link RconClientException} when the specified password is incorrect.
+ */
 public class AuthFailureException extends RconClientException {
     public AuthFailureException() {
         super("Authentication failure");
